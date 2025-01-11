17d777faf8a7f858896561b5407e3da708f6a6d7
yctai1994 <yctai.git@gmail.com>
2025-01-12 04:44:47 +0800

    A better Householder transformation.

--- Hessenberg.jl
+++ Hessenberg.jl
@@ -11,8 +11,7 @@ import LinearAlgebra as la
 const A0 = [ 12.0 -51.0 4.0; 6.0 167.0 -68.0; -4.0 24.0 -41.0; ]
 
 # ╔═╡ 3515d200-b8ef-4149-8b59-d2dfffeb6ac9
-begin
-	A = copy(A0)
+let A = copy(A0)
 	N = size(A, 2)
 	n = Vector{Float64}(undef, N)
 
@@ -30,7 +29,7 @@ begin
 			nk[i] /= nk_norm
 		end
 	
-		for j in k:size(A, 2)
+		for j in k:N
 			tmp = la.dot(nk, @view(A[k:N,j]))
 			for i in k:N
 				A[i,j] -= 2.0 * n[i] * tmp
@@ -40,6 +39,57 @@ begin
 
 	A
 end
+
+# ╔═╡ a01dc808-324d-4c00-bcf5-153ad644db15
+my_R, my_Q = let R = copy(A0), Q = similar(R)
+	N = size(R, 2)
+
+	@inbounds for j in 1:N
+		for i in 1:N
+			Q[i,j] = 0.0
+		end
+		@inbounds Q[j,j] = 1.0
+	end
+
+	for k in 1:N-1
+		scale = maximum(abs, @view(R[k:N,k]))
+		@inbounds for i in k:N
+			R[i,k] /= scale
+		end
+
+		alpha = la.norm(@view(R[k:N,k]))
+		R[k,k] += alpha
+		alpha_beta = alpha * R[k,k]
+
+		for j in k+1:N
+			tmp = la.dot(@view(R[k:N,k]), @view(R[k:N,j]))
+			tmp /= alpha_beta
+			for i in k:N
+				R[i,j] -= tmp * R[i,k]
+			end
+		end
+
+		for j in 1:N
+			tmp = la.dot(@view(R[k:N,k]), @view(Q[k:N,j]))
+			tmp /= alpha_beta
+			for i in k:N
+				Q[i,j] -= tmp * R[i,k]
+			end
+		end
+
+		R[k,k] = -scale * alpha
+	end
+
+	R, Q
+end
+
+# ╔═╡ 1429728d-aa2e-4a22-8cca-4529cd3d1a6f
+ref_R, ref_Q = let QR = la.qr(copy(A0))
+	QR.R, QR.Q
+end
+
+# ╔═╡ 763a2e06-d6ad-4a6f-8c3f-e4c2fbcfd273
+my_Q * ref_Q
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -88,5 +138,8 @@ version = "5.11.0+0"
 # ╠═a5881ab9-053c-47ac-b23c-da051ecc171d
 # ╠═109c3c7c-ceb2-11ef-3483-89b230552d1f
 # ╠═3515d200-b8ef-4149-8b59-d2dfffeb6ac9
+# ╠═a01dc808-324d-4c00-bcf5-153ad644db15
+# ╠═1429728d-aa2e-4a22-8cca-4529cd3d1a6f
+# ╠═763a2e06-d6ad-4a6f-8c3f-e4c2fbcfd273
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
